565ba1862942bd2867d1b197b5bfb077c763bd8c
Attik Zacharia <zacharia.attik@outlook.com>
2015-12-13 23:43:14 +0100

    ajout structure de base
    ajout du squelette de l'application

---
+++ src/app.js
@@ -0,0 +1,68 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+
+var main = new UI.Card({
+  title: 'My-Help',
+  titleColor: 'blue',
+  icon: 'images/menu_icon.png',
+  subtitle: "Vous avez besoin d'aide ?",
+  body: 'Appuyez sur un bouton.',
+  subtitleColor: 'indigo', // Named colors
+  bodyColor: '#9a0036' // Hex colors
+});
+
+main.show();
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'My-Help',
+        titleColor: 'blue',
+        icon: 'images/menu_icon.png',
+        subtitle: 'ligne à proximité'
+      }, {
+        title: 'ligne 9',
+        subtitle: '2 min'
+      },
+         {
+           title: 'Ligne A',
+           subtitle: '6min'
+      }]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
+
+main.on('click', 'select', function(e) {
+  var wind = new UI.Window({
+    fullscreen: true,
+  });
+  var textfield = new UI.Text({
+    position: new Vector2(0, 65),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'activer fonction de recherche',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
+});
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('A Card');
+  card.subtitle('Is a Window');
+  card.body('pourrait switcher les horaires');
+  card.show();
+});
